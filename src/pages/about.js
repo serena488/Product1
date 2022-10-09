f78865486fcc8eb83597e21f80e288f34ce9f649
Abdullahishtiaq488 <abdullahishtiaq488@gmail.com>
2022-10-09 20:12:41 +0500

    changes

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,8 @@
 import { Col, Container, Row } from "react-bootstrap";
-import "../about.css"
+import "../about.css";
+import Jango from "../assets/img/jango-truck.jpg";
+import Jangol from "../assets/img/jangol-truck.jpg";
+import { GiCutDiamond } from "@react-icons/all-files/gi/GiCutDiamond.esm";
 
 const About = () => {
   return (
@@ -14,7 +17,36 @@ const About = () => {
           <h2>About Our Comapany</h2>
         </div>
         <Container className="mt-4">
-        <hr />
+          <hr />
+          <Row className="mt-4 my-4">
+            <Col sm={12} md={5} sm={5}> <p>We have multiple dispatch service plans to choose from.
+              Imagine never having to endlessly search load boards or fill out and fax paperwork again. We take care of the busy work so you can do what your paid to do – drive.
+              Bottom line, our dispatch services help save truckers time & money.
+              We provide options on our flat-rate fees. You know what you want so we provide three separate plans to choose from.
+              A Paperwork only, Basic and Professional plan.
+              So why pay another trucking dispatch service 10% or more on every load? Because of this,
+              Truckage Dispatch Services came up with a ‘one stop shop’ for all your trucking dispatch needs.</p></Col>
+            <Col sm={12} md={7} sm={7} ><img className="jango" src={Jango} /></Col>
+          </Row>
+          <Row className="mt-4 my-4">
+            <Col sm={12} md={7} sm={7}>
+              <img src={Jangol} ></img>
+            </Col>
+            <Col sm={12} md={5} sm={5}>
+              <div className="container p-2 mt-4 my-2 align-top myS">
+                <h3>Our Mission</h3>
+              </div>
+              <hr />
+              <p>Our mission at Truckage Dispatch is to provide an unparalleled level of service to our clients by building
+                strong relationships with our clients and aiding them in running a prosperous business.</p></Col>
+          </Row>
+          <Row className="mt-4 my-4 text-center">
+            <GiCutDiamond className="mt-4 my-4" fontSize="8em" color="orangered" /><br />
+            <h1>Our Core Values</h1>
+            <p>We deliver the competitive advantage you need while eliminating the uncertainty and complexity of your freight delivery. Our service allows you to focus on your business.
+              Truckage dispatcher is assigned to every relationship, becoming a single point of contact</p>
+            
+          </Row>
         </Container>
 
       </div>
--- src/pages/faqs.js
+++ src/pages/faqs.js
@@ -14,90 +14,63 @@ const FAQS = () => {
                 </div>
             </div>
             <Container>
-                
+
                 <div className="container p-2 mt-4 align-top my-4 myS">
                     <p>FAQ's</p>
                     <h2>Let's Solve your Queries...</h2>
                 </div>
                 <hr className="my-4" />
                 <Container className="border text-center mt-4">
-                    
-                <Accordion flush className="mt-4 my-4">
 
-                    <Accordion.Item  eventKey="0">
+                    <Accordion flush className="mt-4 my-4">
 
-                        <Accordion.Header><h4><FaQuora className="me-2" color="orangered" fontSize="27px" />: Accordion Item</h4></Accordion.Header>
-                        <Accordion.Body className="bodys">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item  eventKey="1">
-                        <Accordion.Header ><h4><FaQuora className="me-2" color="orangered" fontSize="27px" />: Accordion Item</h4></Accordion.Header>
-                        <Accordion.Body className="bodys">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item  eventKey="2">
-                        <Accordion.Header ><h4><FaQuora className="me-2" color="orangered" fontSize="27px" />: Accordion Item</h4></Accordion.Header>
-                        <Accordion.Body className="bodys">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item eventKey="3">
-                        <Accordion.Header ><h4><FaQuora className="me-2" color="orangered" fontSize="27px" />: Accordion Item</h4></Accordion.Header>
-                        <Accordion.Body className="bodys">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item  eventKey="4">
-                        <Accordion.Header ><h4><FaQuora className="me-2" color="orangered" fontSize="27px" />: Accordion Item</h4></Accordion.Header>
-                        <Accordion.Body className="bodys">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                    <Accordion.Item  eventKey="5">
-                        <Accordion.Header ><h4><FaQuora className="me-2" color="orangered" fontSize="27px" />: Accordion Item</h4></Accordion.Header>
-                        <Accordion.Body className="bodys">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-                            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-                            aliquip ex ea commodo consequat. Duis aute irure dolor in
-                            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                            culpa qui officia deserunt mollit anim id est laborum.
-                        </Accordion.Body>
-                    </Accordion.Item>
-                </Accordion>
+                        <Accordion.Item eventKey="0">
+
+                            <Accordion.Header><h5><FaQuora className="me-2" color="orangered" fontSize="27px" />: Do I need to fill out the contract to start working with you?
+                            </h5></Accordion.Header>
+                            <Accordion.Body className="bodys">
+                                Yes, the contract is mandatory to start working with us, it’s a super easy contract and sheet
+                                which you need to fill out, as per the details that you will mention we’ll find you the loads
+                                accordingly
+
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="1">
+                            <Accordion.Header ><h5><FaQuora className="me-2" color="orangered" fontSize="27px" />:  What happens If I don’t want to take the load that you have offered?</h5></Accordion.Header>
+                            <Accordion.Body className="bodys">
+                                We know that you may not like every load, and you don’t need to stick with that since we are not
+                                forcing you to take the load as there is no forced dispatch, we will start finding you another load
+                                that is acceptable to you.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="2">
+                            <Accordion.Header ><h5><FaQuora className="me-2" color="orangered" fontSize="27px" />:  Do I need to fill out any broker-created packages for brokers I have never done business
+                                with before?</h5></Accordion.Header>
+                            <Accordion.Body className="bodys">
+                                No, you just need to sit and relax, we will fill all the packages for you on your behalf.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="3">
+                            <Accordion.Header ><h5><FaQuora className="me-2" color="orangered" fontSize="27px" />: Do you have any knowledge of what brokers are legit (Good or Bad)?</h5></Accordion.Header>
+                            <Accordion.Body className="bodys">
+                            We only do business with brokers who are reputable and who we know will pay you.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="4">
+                            <Accordion.Header ><h5><FaQuora className="me-2" color="orangered" fontSize="27px" />: What do you require from me to start working?</h5></Accordion.Header>
+                            <Accordion.Body className="bodys">
+                                We just need three documents from your side, a copy of your proof of insurance, your W9, your
+                                Mc authority paper, and a Complete carrier agreement.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                        <Accordion.Item eventKey="5">
+                            <Accordion.Header ><h5><FaQuora className="me-2" color="orangered" fontSize="27px" />: How can I get paid?</h5></Accordion.Header>
+                            <Accordion.Body className="bodys">
+                                If you have a factoring company that is great and if not, the broker will pay you directly using
+                                quick pay, and how quickly you get paid it depends upon the broker.
+                            </Accordion.Body>
+                        </Accordion.Item>
+                    </Accordion>
                 </Container>
 
             </Container>
