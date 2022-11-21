9930aeefae397ce8610725f565978f62996013a9
Abdullahishtiaq488 <abdullahishtiaq488@gmail.com>
2022-11-21 08:55:24 +0500

    changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -17,40 +17,42 @@ import { RiArrowRightSFill } from "@react-icons/all-files/ri/RiArrowRightSFill.e
 export const Footer = () => {
   return (
     <section className="Footer">
-      
-        <footer className="footer-32892 pb-0">
-          <div className="site-section">
-            <div className="container">
-              <div className="Img5 my-4">
-                <img src={Img5} />
+      <footer className="footer-32892 pb-0">
+        <div className="site-section">
+          <div className="container">
+            <div className="Img5 my-4">
+              <img src={Img5} />
+            </div>
+
+            <div className="row">
+              <div className="col-md pr-md-5 mb-4 m-2 mb-md-0">
+                <h3>ABOUT US</h3>
+                <p className="mb-4">
+                  We are Truckage Dispatch Services. Experienced trucking
+                  company operating in 48 U.S continental states. We are an
+                  Illinois asset based over the road transportation company. We
+                  service all 48 states, and specialize in expedite full
+                  truckload and LTL freight. We are a service based company, we
+                  sell our transportation services to shippers, customers,
+                  brokers, etc. We also can provide dedicated service for your
+                  dedicated lanes if needed.
+                </p>
+                <ul className="list-unstyled quick-info mb-4 fs-2">
+                  <li>
+                    <u>
+                      <b>Call:</b>
+                    </u>{" "}
+                    +1 (980) 485-5274
+                  </li>
+                  <li>
+                    <u>
+                      <b>Email:</b>
+                    </u>{" "}
+                    truckage.dispatch@gmail.com
+                  </li>
+                </ul>
               </div>
-
-              <div className="row">
-                <div className="col-md pr-md-5 mb-4 m-2 mb-md-0">
-                  <h3>ABOUT US</h3>
-                  <p className="mb-4">
-                    We are Truckage Dispatch Services. Experienced trucking
-                    company operating in 48 U.S continental states. We are an
-                    Illinois asset based over the road transportation company.
-                    We service all 48 states, and specialize in expedite full
-                    truckload and LTL freight. We are a service based company,
-                    we sell our transportation services to shippers, customers,
-                    brokers, etc. We also can provide dedicated service for your
-                    dedicated lanes if needed.
-                  </p>
-                  <ul className="list-unstyled quick-info mb-4 fs-2">
-                    <li>
-                      <u><b>Call:</b></u> +1 (980) 485-5274
-                      
-                    </li>
-                    <li>
-                     <u><b>Email:</b></u> truckage.dispatch@gmail.com
-                    </li>
-                  </ul>
-
-                  
-                </div>
-                <div className="col-md mb-4 m-2 mb-md-0">
+              {/* <div className="col-md mb-4 m-2 mb-md-0">
                   <h3> LATEST TWEETS</h3>
 
                   <ul className="list-unstyled tweets">
@@ -86,78 +88,79 @@ export const Footer = () => {
                       </div>
                     </li>
                   </ul>
-                </div>
+                </div> */}
 
-                <div className="col-md-3 mb-4 m-2 mb-md-0">
-                  <h3> INSTAGRAM</h3>
-                  <div className="row gallery">
-                    <div className="col-6">
-                      <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
-                        <img src={Img4} alt="Image" className="img-fluid" />
-                      </a>
-                      <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
-                        <img src={Img1} alt="Image" className="img-fluid" />
-                      </a>
-                    </div>
-                    <div className="col-6">
-                      <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
-                        <img src={Img2} alt="Image" className="img-fluid" />
-                      </a>
-                      <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
-                        <img src={Img3} alt="Image" className="img-fluid" />
-                      </a>
-                    </div>
+              <div className="col-md-3 mb-4 m-2 mb-md-0">
+                <h3> INSTAGRAM</h3>
+                <div className="row gallery">
+                  <div className="col-6">
+                    <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
+                      <img src={Img4} alt="Image" className="img-fluid" />
+                    </a>
+                    <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
+                      <img src={Img1} alt="Image" className="img-fluid" />
+                    </a>
                   </div>
-                  <br />
-                  <br />
-                  <br />
-                </div>
-
-                <h3 className="text-start mt-4 ms-4">Follow Us</h3>
-                <hr />
-                <Container className="followup">
-                  <Row className="followdown">
-                    <a href="https://www.facebook.com/Tds.dispatchservices" className="btn btn5">
-                      <FaFacebook
-                        
-                        className="btncolor"
-                        fontSize="24px"
-                      />
+                  <div className="col-6">
+                    <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
+                      <img src={Img2} alt="Image" className="img-fluid" />
                     </a>
-
-                    <buttons className="btn btn5">
-                      <FaTwitter className="btncolor" fontSize="24px" />
-                    </buttons>
-
-                    <a className="btn btn5"
-                    href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
-                      <FaInstagram className="btncolor" fontSize="24px" />
+                    <a href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y=">
+                      <img src={Img3} alt="Image" className="img-fluid" />
                     </a>
-
-                    <buttons className="btn btn5">
-                      <FaYoutube className="btncolor" fontSize="24px" />
-                    </buttons>
-
-                    <a href="https://www.linkedin.com/company/truckage-dispatch-services/" className="btn btn5">
-                      <FaLinkedin className="btncolor" fontSize="24px" />
-                    </a>
-                  </Row>
-                </Container>
-
-                <hr />
-
-                <div className="col-12">
-                  <div className="py-5 footer-menu-wrap d-md-flex align-items-center">
-                    © 2020 Copyright: Some Rights Reserved (2009-2022) under
-                    Creative Commons Attribution-NonCommercial-ShareAlike
-                    license unless otherwise noted.
                   </div>
+                </div>
+                <br />
+                <br />
+                <br />
+              </div>
+
+              <h3 className="text-start mt-4 ms-4">Follow Us</h3>
+
+              <Container className="followup">
+                <Row className="followdown">
+                  <a
+                    href="https://www.facebook.com/Tds.dispatchservices"
+                    className="btn btn5"
+                  >
+                    <FaFacebook className="btncolor" fontSize="24px" />
+                  </a>
+
+                  <buttons className="btn btn5">
+                    <FaTwitter className="btncolor" fontSize="24px" />
+                  </buttons>
+
+                  <a
+                    className="btn btn5"
+                    href="https://instagram.com/truckage_dispatch?igshid=YmMyMTA2M2Y="
+                  >
+                    <FaInstagram className="btncolor" fontSize="24px" />
+                  </a>
+
+                  <buttons className="btn btn5">
+                    <FaYoutube className="btncolor" fontSize="24px" />
+                  </buttons>
+
+                  <a
+                    href="https://www.linkedin.com/company/truckage-dispatch-services/"
+                    className="btn btn5"
+                  >
+                    <FaLinkedin className="btncolor" fontSize="24px" />
+                  </a>
+                </Row>
+              </Container>
+
+              <div className="col-12">
+                <div className="py-5 footer-menu-wrap d-md-flex align-items-center">
+                  © 2020 Copyright: Some Rights Reserved (2009-2022) under
+                  Creative Commons Attribution-NonCommercial-ShareAlike license
+                  unless otherwise noted.
                 </div>
               </div>
             </div>
           </div>
-        </footer>
-      
+        </div>
+      </footer>
     </section>
   );
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -7,54 +7,54 @@ import { GiCutDiamond } from "@react-icons/all-files/gi/GiCutDiamond.esm";
 const About = () => {
   return (
     <section className="About" id="aboutus">
-      
+
       <div className="Aboutbanner">
         <h1>Who are We...</h1>
       </div>
       <Container>
-      <div className="container">
-        <div className="container p-2 mt-4 align-top myS">
-          <p className="text-orangered">ABOUT US</p>
+        <div className="container">
+          <div className="container p-2 mt-4 align-top myS">
+            <p className="text-orangered">ABOUT US</p>
 
-          <h2>About Our Comapany</h2>
+            <h2>About Our Comapany</h2>
+          </div>
+
+          <Container className="mt-4">
+            <hr />
+            <Row className="mt-4 my-4">
+              <Col sm={12} md={12} lg={5}> <p>Truckage Dispatch Services is the US's most trusted independent freight dispatching company.
+                TDS offers highly paying loads by targeting hot lanes to owner-operators and carriers to enhance
+                their revenues. TDS Sales & Dispatch Team are well experienced. With our expertise and years
+                of experience, we have built a good reputation in freight dispatching.</p></Col>
+              <Col sm={12} md={12} lg={7} ><img className="jango" src={Jango} /></Col>
+            </Row>
+            <Row className="mt-4 my-4">
+              <Col sm={12} md={12} lg={7}>
+                <img src={Jangol} ></img>
+              </Col>
+              <Col sm={12} md={12} lg={5}>
+                <div className="container p-2 mt-4 my-2 align-top myS">
+                  <h3>Our Mission</h3>
+                </div>
+                <hr />
+                <p>TDS understands how frustrating and hard to negotiate with the brokers and handle long
+                  paperwork, billing, and Logistics, this is why TDS helps Owner Operators and Carriers to just
+                  focus on picking up the load and delivering it. Rest of the things that TDS cover is that we will handle
+                  your paperwork, billing, back office, and logistics to make your move smooth, which leads you
+                  to just focus on the road and make good money, Our mission is to offer full-time dispatch
+                  support so that syou can earn more at your ease.</p></Col>
+            </Row>
+            <Row sm={12} md={12} lg={12} className="mt-4 my-4 text-center">
+              <GiCutDiamond className="mt-4 my-4" fontSize="8em" color="orangered" /><br />
+              <h1>Our Core Values</h1>
+              <p>We deliver the competitive advantage you need while eliminating the uncertainty and complexity of your freight delivery. Our service allows you to focus on your business.
+                Truckage dispatcher is assigned to every relationship, becoming a single point of contact</p>
+
+            </Row>
+          </Container>
+
+
         </div>
-        
-        <Container className="mt-4">
-          <hr />
-          <Row className="mt-4 my-4">
-            <Col sm={12} md={12} lg={5}> <p>Truckage Dispatch Services is the US's most trusted independent freight dispatching company.
-              TDS offers highly paying loads by targeting hot lanes to owner-operators and carriers to enhance
-              their revenues. TDS Sales & Dispatch Team are well experienced. With our expertise and years
-              of experience, we have built a good reputation in freight dispatching.</p></Col>
-            <Col sm={12} md={12} lg={7} ><img className="jango" src={Jango} /></Col>
-          </Row>
-          <Row className="mt-4 my-4">
-            <Col sm={12} md={12} lg={7}>
-              <img src={Jangol} ></img>
-            </Col>
-            <Col sm={12} md={12} lg={5}>
-              <div className="container p-2 mt-4 my-2 align-top myS">
-                <h3>Our Mission</h3>
-              </div>
-              <hr />
-              <p>TDS understands how frustrating and hard to negotiate with the brokers and handle long
-                paperwork, billing, and Logistics, this is why TDS helps Owner Operators and Carriers to just
-                focus on picking up the load and delivering it. Rest of the things TDS we cover, we will handle
-                your paperwork, billing, back office, and logistics to make your move smooth, which leads you
-                to just focus on the road and make good money, Our mission is to offer full-time dispatch
-                support to so you can earn more at your ease.</p></Col>
-          </Row>
-          <Row sm={12} md={12} lg={12} className="mt-4 my-4 text-center">
-            <GiCutDiamond className="mt-4 my-4" fontSize="8em" color="orangered" /><br />
-            <h1>Our Core Values</h1>
-            <p>We deliver the competitive advantage you need while eliminating the uncertainty and complexity of your freight delivery. Our service allows you to focus on your business.
-              Truckage dispatcher is assigned to every relationship, becoming a single point of contact</p>
-
-          </Row>
-        </Container>
-        
-
-      </div>
       </Container>
     </section>)
 }
